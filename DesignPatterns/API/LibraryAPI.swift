e4163eed7b488aaa967ec4422ec91ff4cfc5c504
Matthew Deuschle <mdeuschle@gmail.com>
2019-08-18 12:01:06 -0700

    update contraints

---
+++ DesignPatterns/API/LibraryAPI.swift
@@ -0,0 +1,15 @@
+//
+//  LibraryAPI.swift
+//  DesignPatterns
+//
+//  Created by Matt Deuschle on 8/4/19.
+//  Copyright © 2019 Matt Deuschle. All rights reserved.
+//
+
+import Foundation
+
+struct LibraryAPI {
+    private init() {}
+    static let shared = LibraryAPI()
+    
+}
